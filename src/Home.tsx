6915653aac0cd787fafc69c7e48114830ade2222
Harry177 <harry177@rambler.ru>
2023-03-18 02:50:09 +0100

    feat: add search bar with opportunity to save input value in localstorage

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Header } from './Header';
+import { SearchBar } from './SearchBar';
 
 export class Home extends React.Component {
   render() {
     return (
       <div className="container">
         <Header page="Main" />
+        <SearchBar />
       </div>
     );
   }
---
+++ src/SearchBar.tsx
@@ -0,0 +1,23 @@
+import React, { ChangeEvent } from 'react';
+
+export class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = (event: ChangeEvent) => {
+    this.setState({ term: (event.target as HTMLInputElement).value });
+    localStorage.setItem('input', (event.target as HTMLInputElement).value);
+  };
+
+  render() {
+    return (
+      <div className="search-bar">
+        <form className="search-form">
+          <div className="search-field">
+            <label>Search</label>
+            <input type="text" value={this.state.term} onChange={this.onInputChange} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
