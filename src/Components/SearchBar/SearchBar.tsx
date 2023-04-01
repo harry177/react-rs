8f1cb3ad7e3b06b11f46a8131289fcbfbe8181b9
Harry177 <harry177@rambler.ru>
2023-04-01 04:47:41 +0200

    refactor: rewrite SearchBar component with hooks

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,35 +1,46 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './SearchBarStyles.css';
 
-export class SearchBar extends React.Component {
-  state = { term: localStorage.getItem('input') || '' };
+export const SearchBar = () => {
+  // state = { term: localStorage.getItem('input') || '' };
 
-  componentWillUnmount() {
+  const [state, setState] = useState(localStorage.getItem('input') || '');
+  const initialValue = useRef('');
+
+  /* componentWillUnmount() {
     if (this.state.term) {
       localStorage.setItem('input', this.state.term);
     }
-  }
+  } */
 
-  onInputChange = (event: ChangeEvent) => {
+  useEffect(() => {
+    initialValue.current = state;
+  }, [state]);
+
+  useEffect(() => {
+    return () => {
+      localStorage.setItem('input', initialValue.current);
+    };
+  }, []);
+
+  /* onInputChange = (event: ChangeEvent) => {
     this.setState({ term: (event.target as HTMLInputElement).value });
-  };
+  }; */
 
-  render() {
-    return (
-      <div className="search-bar">
-        <form className="search-form">
-          <div className="search-field">
-            <label className="search-label">Search</label>
-            <input
-              className="search-input"
-              type="text"
-              aria-label="input"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar">
+      <form className="search-form">
+        <div className="search-field">
+          <label className="search-label">Search</label>
+          <input
+            className="search-input"
+            type="text"
+            aria-label="input"
+            value={state}
+            onChange={(event: ChangeEvent) => setState((event.target as HTMLInputElement).value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
